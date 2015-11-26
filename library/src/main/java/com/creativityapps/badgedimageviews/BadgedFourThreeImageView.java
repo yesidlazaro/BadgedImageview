68e1d9a1f984a69d78cfd91dca076768ac8fb6d5
Hector de Isidro <hector.isidro@mobusi.com>
2015-11-26 16:20:50 +0100

    Remove duplicate code

--- library/src/main/java/com/creativityapps/badgedimageviews/BadgedFourThreeImageView.java
+++ library/src/main/java/com/creativityapps/badgedimageviews/BadgedFourThreeImageView.java
@@ -17,88 +17,21 @@
 package com.creativityapps.badgedimageviews;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
-import android.support.annotation.ColorInt;
 import android.util.AttributeSet;
-import android.view.Gravity;
 
 
 /**
  * A view group that draws a badge drawable on top of it's contents.
  */
-public class BadgedFourThreeImageView extends FourThreeImageView {
-
-    private Drawable badge;
-    private boolean drawBadge;
-    private boolean badgeBoundsSet = false;
-    private int badgeGravity;
-    private int badgePadding;
-    private String badgeText;
-    private int badgeColor;
-
+public class BadgedFourThreeImageView extends BadgedImageView {
     public BadgedFourThreeImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.BadgedImageView, 0, 0);
-        badgeGravity = a.getInt(R.styleable.BadgedImageView_badgeGravity, Gravity.END | Gravity
-                .BOTTOM);
-        badgePadding = a.getDimensionPixelSize(R.styleable.BadgedImageView_badgePadding, 0);
-        badgeText=a.getString(R.styleable.BadgedImageView_badgeText);
-        badgeColor=a.getColor(R.styleable.BadgedImageView_badgeColor,Color.WHITE);
-        badge = new GifBadge(context,badgeText,badgeColor);
-
-        a.recycle();
-    }
-
-    public void showBadge(boolean show) {
-        drawBadge = show;
-    }
-
-    public void setBadgeColor(@ColorInt int color) {
-        badgeColor=color;
-        badge.setColorFilter(badgeColor, PorterDuff.Mode.SRC_IN);
-    }
-
-    public void setBadgeText(String newText) {
-        this.badgeText=newText;
-        badge=new GifBadge(getContext(),badgeText,badgeColor);
-        invalidate();
-    }
-
-    public boolean isBadgeVisible() {
-        return drawBadge;
-    }
-    @Override
-    public void draw(Canvas canvas) {
-        super.draw(canvas);
-        if (drawBadge) {
-            if (!badgeBoundsSet) {
-                layoutBadge();
-            }
-            badge.draw(canvas);
-        }
     }
 
     @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        layoutBadge();
-    }
-
-    private void layoutBadge() {
-        Rect badgeBounds = badge.getBounds();
-        Gravity.apply(badgeGravity,
-                badge.getIntrinsicWidth(),
-                badge.getIntrinsicHeight(),
-                new Rect(0, 0, getWidth(), getHeight()),
-                badgePadding,
-                badgePadding,
-                badgeBounds);
-        badge.setBounds(badgeBounds);
-        badgeBoundsSet = true;
+    protected void onMeasure(int widthSpec, int heightSpec) {
+        int fourThreeHeight = MeasureSpec.makeMeasureSpec(MeasureSpec.getSize(widthSpec) * 3 / 4,
+                MeasureSpec.EXACTLY);
+        super.onMeasure(widthSpec, fourThreeHeight);
     }
 }
--- library/src/main/java/com/creativityapps/badgedimageviews/BadgedSquareImageView.java
+++ library/src/main/java/com/creativityapps/badgedimageviews/BadgedSquareImageView.java
@@ -17,88 +17,19 @@
 package com.creativityapps.badgedimageviews;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
-import android.support.annotation.ColorInt;
 import android.util.AttributeSet;
-import android.view.Gravity;
 
 
 /**
  * A view group that draws a badge drawable on top of it's contents.
  */
-public class BadgedSquareImageView extends SquareImageView {
-
-    private Drawable badge;
-    private boolean drawBadge;
-    private boolean badgeBoundsSet = false;
-    private int badgeGravity;
-    private int badgePadding;
-    private String badgeText;
-    private int badgeColor;
-
+public class BadgedSquareImageView extends BadgedImageView {
     public BadgedSquareImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.BadgedImageView, 0, 0);
-        badgeGravity = a.getInt(R.styleable.BadgedImageView_badgeGravity, Gravity.END | Gravity
-                .BOTTOM);
-        badgePadding = a.getDimensionPixelSize(R.styleable.BadgedImageView_badgePadding, 0);
-        badgeText=a.getString(R.styleable.BadgedImageView_badgeText);
-        badgeColor=a.getColor(R.styleable.BadgedImageView_badgeColor,Color.WHITE);
-        badge = new GifBadge(context,badgeText,badgeColor);
-
-        a.recycle();
-    }
-
-    public void showBadge(boolean show) {
-        drawBadge = show;
-    }
-
-    public void setBadgeColor(@ColorInt int color) {
-        badgeColor=color;
-        badge.setColorFilter(badgeColor, PorterDuff.Mode.SRC_IN);
-    }
-
-    public void setBadgeText(String newText) {
-        this.badgeText=newText;
-        badge=new GifBadge(getContext(),badgeText,badgeColor);
-        invalidate();
-    }
-
-    public boolean isBadgeVisible() {
-        return drawBadge;
-    }
-    @Override
-    public void draw(Canvas canvas) {
-        super.draw(canvas);
-        if (drawBadge) {
-            if (!badgeBoundsSet) {
-                layoutBadge();
-            }
-            badge.draw(canvas);
-        }
     }
 
     @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        layoutBadge();
-    }
-
-    private void layoutBadge() {
-        Rect badgeBounds = badge.getBounds();
-        Gravity.apply(badgeGravity,
-                badge.getIntrinsicWidth(),
-                badge.getIntrinsicHeight(),
-                new Rect(0, 0, getWidth(), getHeight()),
-                badgePadding,
-                badgePadding,
-                badgeBounds);
-        badge.setBounds(badgeBounds);
-        badgeBoundsSet = true;
+    protected void onMeasure(int widthSpec, int heightSpec) {
+        super.onMeasure(widthSpec, heightSpec);
     }
 }
